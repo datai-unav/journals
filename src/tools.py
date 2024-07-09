f0aae62eed1137ab126534c5a797fcc339ee414e
Leandro Gonzalez <leandroglez39@gmail.com>
2024-07-09 13:54:49 +0200

    Refactor code to improve efficiency and readability

--- src/tools.py
+++ src/tools.py
@@ -3,6 +3,29 @@ import pandas as pd
 from tqdm import tqdm
 
 
+
+def map_pmid_to_journal(df, info_j):
+    
+    df_output = pd.DataFrame(columns=['PMID', 'Date', 'Title', 'Authors', 'Affiliation', 'Mail', 'Journal'])
+    rows = list(df.iterrows())
+    for index, row in tqdm(rows):
+        data = row.to_dict()
+        
+        for jn in info_j:
+            if str(data['PMID']) in jn['additional_info']:
+                data['Journal'] = jn['name']
+                temp_df = pd.DataFrame(data, index=[0])
+                df_output = pd.concat([df_output, temp_df], ignore_index=True)                
+                break 
+    
+    df_output.to_csv('data/journals_results_names.csv', index=False)    
+
+
+def unique_mail(df: pd.DataFrame):
+
+    # Get the unique mail
+    print(df['Mail'].value_counts())
+
 if __name__ == '__main__':
     # Load the model
     with open('data/info_ids.pkl', 'rb') as file:
@@ -17,7 +40,7 @@ if __name__ == '__main__':
     with open('data/info_journals.pkl', 'rb') as file:
         info_j = pickle.load(file)
         
-    print(info_j[0].keys())
+    
     # # a =  [x["name"] for x in info_j]
     
     # # # Sort by ascending by letter
@@ -30,8 +53,12 @@ if __name__ == '__main__':
     
     
     # Load a dataframe in csv file with headers
-    df = pd.read_csv('data/journals_results.csv')
-    
+    df = pd.read_csv('data/journals_results_filtered.csv')
+
+    # Count the unique values in the Mail column, the Mail column is a list
+  
+
+    #df.to_csv('data/journals_results_filtered.csv', index=False)
     # # Remove the rows with [] value en Mail column
     # df = df[df['Mail'] != '[]']
     
@@ -39,17 +66,4 @@ if __name__ == '__main__':
     
     # df.to_csv('data/journals_results_filtered.csv', index=False)
     
-    df_output = pd.DataFrame(columns=['PMID', 'Date', 'Title', 'Authors', 'Affiliation', 'Mail', 'Journal'])
-    rows = list(df.iterrows())
-    for index, row in tqdm(rows[0:10]):
-        data = row.to_dict()
-        
-        for jn in info_j:
-            if data['PMID'] in jn['additional_info']:
-                data['Journal'] = jn['name']
-                temp_df = pd.DataFrame(data, index=[0])
-                df_output = df.concat(df_output, temp_df)                
-                break 
-    
-    df_output.to_csv('data/journals_results_names.csv', index=False)    
-    
+   
