d79770ae28822a57404fa6a447d9254699992c7c
Leandro Gonzalez <leandroglez39@gmail.com>
2024-07-13 14:16:47 +0200

    chore: Update test.py to load and print HTML content from PubMed using Selenium

--- src/test.py
+++ src/test.py
@@ -9,43 +9,55 @@ from selenium.webdriver.chrome.options import Options
 
 if __name__ == '__main__':
     
+
+    data = pickle.load(open(r'data\journals\eLife\elif\info_ids.pkl', 'rb'))
+    print(len(data))
     # Load the model
     # with open('info_ids.pkl', 'rb') as file:
     #     model = pickle.load(file)
     
-    with open(r'data\journals\num_processed.pkl', 'rb') as file:
-        num_processed = pickle.load(file)
+    # with open(r'data\journals\num_processed.pkl', 'rb') as file:
+    #     num_processed = pickle.load(file)
 
     #print(num_processed)
 
     # with open('errors_ids.pkl', 'rb') as file:
     #     errors = pickle.load(file)
     
-    with open('data\info_journals.pkl', 'rb') as file:
+    with open(r'data\info_journals.pkl', 'rb') as file:
         info_j = pickle.load(file)
     
-     # Set Chrome to run headlessly
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Ensure GUI is off
-    chrome_options.add_argument("--disable-gpu")  # Recommended as per documentation
-    chrome_options.add_argument("--no-sandbox")  # Bypass OS security model, REQUIRED for Docker. Comment this line if not using Docker
-    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36")
-
-
-    driver = webdriver.Chrome(options=chrome_options)  # Initialize Chrome driver
+    #Export Unique names from info_journals
+    # unique_names = set()
+    # for journal in info_j:
+    #     unique_names.add(journal['name'])
     
-    url = 'https://pubmed.ncbi.nlm.nih.gov/' + str(30242045) + '/?format=pubmed'
-    driver.get(url)
+    # with open(r'data\unique_names.csv', 'w') as file:
+    #     for name in unique_names:
+    #         file.write(name + '\n')
+
+    #  # Set Chrome to run headlessly
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")  # Ensure GUI is off
+    # chrome_options.add_argument("--disable-gpu")  # Recommended as per documentation
+    # chrome_options.add_argument("--no-sandbox")  # Bypass OS security model, REQUIRED for Docker. Comment this line if not using Docker
+    # chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+    # chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36")
+
+
+    # driver = webdriver.Chrome(options=chrome_options)  # Initialize Chrome driver
+    
+    # url = 'https://pubmed.ncbi.nlm.nih.gov/' + str(30242045) + '/?format=pubmed'
+    # driver.get(url)
 
     
-    # WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "article-details")))
-    # search_results = driver.find_element(By.CLASS_NAME, "article-details").text
-    # data = {"Journal": 'eLife', "Num": 30242045, "Content": search_results}
-    # Print the entire HTML content of the page
-    html_content = driver.page_source
-    print(html_content)
+    # # WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CLASS_NAME, "article-details")))
+    # # search_results = driver.find_element(By.CLASS_NAME, "article-details").text
+    # # data = {"Journal": 'eLife', "Num": 30242045, "Content": search_results}
+    # # Print the entire HTML content of the page
+    # html_content = driver.page_source
+    # print(html_content)
 
-    # Close the driver after inspection
-    driver.quit()
-    #print(data)
+    # # Close the driver after inspection
+    # driver.quit()
+    # #print(data)
