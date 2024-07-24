9cea95a08ee319039e491114a8d22c6caf74c032
Leandro Gonzalez Montesino <leandroglez39@gmail.com>
2024-07-24 10:15:27 +0200

    chore: Split text file into multiple parts

--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,6 @@
 import pickle
 import pandas as pd
-#from tqdm import tqdm
+from tqdm import tqdm
 
 
 
@@ -26,7 +26,64 @@ def unique_mail(df: pd.DataFrame):
     # Get the unique mail
     print(df['Mail'].value_counts())
 
+
+def detect_missing_journals():
+    
+    
+        
+    
+    df = pd.read_csv('data/journals_completed.csv')
+    
+    
+    with open('data/unique_names.csv', 'r') as file:
+        names = file.readlines()
+        
+        names = [x.strip() for x in names]    
+        
+        names = set(names)
+        
+        to_delete = df[df['Finished'] == 1]
+    
+        to_delete = set(to_delete['Name'].values)
+
+        diff = names.difference(to_delete)
+        
+        print(len(diff))
+        
+        with open('data/m_names.txt', 'w') as file:
+            for x in diff:
+                file.write(x + '\n')   
+        
+def split_txt(filename, parts=6):
+    """Split a text file into a specified number of parts."""
+    
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    
+    # Calculate the number of lines per part
+    lines_per_part = len(lines) // parts
+    extra_lines = len(lines) % parts
+
+    start = 0
+    for i in range(parts):
+        # Determine the end line index for this part
+        # Add an extra line to the first 'extra_lines' parts to distribute any remainder
+        end = start + lines_per_part + (1 if i < extra_lines else 0)
+        
+        # Write the current part to a new file
+        with open(f'data/part_{i+1}.txt', 'w') as part_file:
+            part_file.writelines(lines[start:end])
+        
+        # Update the start index for the next part
+        start = end
+    
+    
+    
+            
 if __name__ == '__main__':
+    
+    split_txt(filename='data/m_names.txt', parts=6)
+    
     # Load the model
     # with open('data/info_ids.pkl', 'rb') as file:
     #     model = pickle.load(file)
@@ -37,11 +94,9 @@ if __name__ == '__main__':
     #print([x if "@" in x["Content"] else None for x in model[0:50]])   
     #print([ x.keys() if "Mail" in x.keys() else None for x in model] )
     
-    with open('data/info_journals.pkl', 'rb') as file:
-        info_j = pickle.load(file)
-     
-     
-    print(info_j[0].keys())   
+    # with open('data/info_journals.pkl', 'rb') as file:
+    #     info_j = pickle.load(file)
+        
     
     # # a =  [x["name"] for x in info_j]
     
@@ -53,14 +108,16 @@ if __name__ == '__main__':
         
     # print([x if "Music" in x["name"] else None for x in info_j])
     
+    # with open('data/unique_names.csv', 'r') as file:
+    #     lines = file.readlines()
+        
     
-    # Load a dataframe in csv file with headers
-    # df = pd.read_csv('data/journals_results_filtered.csv')
-
-
-    # mails = pd.read_csv('data\mails.csv')
-                
-    # print(len(mails['mails'].unique()))
+    # names = [x['name'] for x in info_j]
+    
+    # with open('data/missing_names.txt', 'w') as file:
+    #     for line in lines:
+    #         if line.strip() not in names:
+    #             file.write(line)
     
     
     # Count the unique values in the Mail column, the Mail column is a list
@@ -74,4 +131,4 @@ if __name__ == '__main__':
     
     # df.to_csv('data/journals_results_filtered.csv', index=False)
     
-    
+   
